38884a76ea8b361f46d0d7f69f131d8cce57c15f
konstantin <kostia505@gmail.com>
2025-07-10 14:32:04 +0200

    img bug fixed

--- src/app/components/PartnersLogo.tsx
+++ src/app/components/PartnersLogo.tsx
@@ -5,15 +5,15 @@ export default function PartnersLogo() {
     <>
       <div className="grid grid-cols-3 justify-between border-t">
         <div className="m-auto border-r w-full h-full items-center justify-center flex p-20">
-          <Image src="/logos/ohmi.png" alt="logo1" width={180} height={50} className="opacity-50 hover:opacity-100 transition-all duration-400" />
+          <Image src="/logos/ohmi.png" alt="logo1" width={200} height={50} className="opacity-50 hover:opacity-100 transition-all duration-400 h-auto w-auto" />
         </div>
 
         <div className="m-auto w-full h-full items-center justify-center flex p-20">
-          <Image src="/logos/dekra.png" alt="logo1" width={130} height={50} className="opacity-50 hover:opacity-100 transition-all duration-400" />
+          <Image src="/logos/dekra.png" alt="logo1" width={130} height={50} className="opacity-50 hover:opacity-100 transition-all duration-400 h-auto w-auto" />
         </div>
 
         <div className="m-auto border-l  w-full h-full items-center justify-center flex p-20">
-          <Image src="/logos/RdA.png" alt="logo1" width={180} height={50} className="opacity-50 hover:opacity-100 transition-all duration-400" />
+          <Image src="/logos/RdA.png" alt="logo1" width={200} height={50} className="opacity-50 hover:opacity-100 transition-all duration-400 h-auto w-auto" />
         </div>
 
       </div>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { ReactLenis, useLenis } from 'lenis/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  console.log(ReactLenis, useLenis)
   return <ParallaxProvider scrollAxis="vertical">
     {children}
   </ParallaxProvider>;
