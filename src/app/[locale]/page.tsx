a58aa946c905c7cef0f7a9f79806c1cd6d9b9cc3
konstantin <kostia505@gmail.com>
2025-09-09 00:07:38 +0200

    mobile test2 welcome

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,9 +11,21 @@ export default function Home() {
     <Revealer />
     <GradientMain />
     {/* сделать мобильный элемент */}
-    <div className="relative px-2.5 pb-2.5 hidden max-[600px]:block h-[100svh]">
-      <div className="pt-[85px] h-full">
+    <div className="px-2.5 pb-2.5 hidden max-[600px]:block h-[100svh] text-[rgba(255,250,240,1)]">
+      <div className="relative pt-[85px] h-full">
         <GradientMobile />
+        <div className="absolute top-[85px]">
+          <div className="px-2.5 pt-5 text-[28px] leading-[36px]">Ihr Weg <br />
+            zu neuen Chancen</div>
+          <div className="p-2.5 opacity-50 font-semibold">
+            <p>
+              Kostenlose Erstberatung
+            </p>
+          </div>
+        </div>
+        <div className="absolute bottom-[5px] w-full text-center">
+          <div className="px-2.5 pb-5 font-semibold">Weiter scrollen</div>
+        </div>
       </div>
     </div>
     {/**/}
