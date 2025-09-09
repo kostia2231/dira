40a4423ccad6147eff8ea7e24903913f64be2393
konstantin <kostia505@gmail.com>
2025-09-09 14:32:59 +0200

    main mobile fix3

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         </div>
 
         <div className="absolute bottom-0 p-2.5 w-full text-center flex flex-col gap-2.5 text-[rgba(255,250,240,1)] font-semibold">
-          <div className="bg-[rgba(255,250,240,0.2)] rounded-[15px] p-5 flex flex-col gap-5  py-2.5 brightness-105">
-            <div className="text-left w-full">Willkommen <br /> bei Ihrer Arbeitsvermittlung mit Herz und Verstand!</div>
+          <div className="bg-[rgba(255,250,240,0.2)] rounded-[15px] flex flex-col gap-5 p-2.5 pb-5 brightness-105">
+            {/*<div className="text-left w-full">Willkommen <br /> bei Ihrer Arbeitsvermittlung mit Herz und Verstand!</div>*/}
             <div className="text-left px-5 uppercase p-5 rounded-[5px] bg-[rgba(44,67,102,0.6)] brightness-105 flex gap-2.5 justify-between items-center w-full">
               <div className="flex justify-between items-center w-fit">
                 Jetzt Telefonieren
@@ -38,7 +38,7 @@ export default function Home() {
               <DownArrow />
             </div>
             <div className="text-center px-2.5 flex items-center justify-center gap-2.5">
-              <div>Weiter scrollen</div>
+              <div>Weiter lesen</div>
             </div>
           </div>
         </div>
